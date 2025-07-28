a61895a679bcfd146d36c373df07f52124e8aef7
José Carlos <jcavalcanti008@gmail.com>
2025-07-28 00:34:28 -0300

    Exercicio 4 a (#11)
    * exercicio 3) -d finalizado, combinando filter e mappara obter notas > 8.5
    
    * exercicio 4) -a finalizado, encontrando a primeira tarefa incompleta

---
+++ exercicio-4-a.js
@@ -0,0 +1,15 @@
+//Usando findIndex para encontrar a posição da primeira tarefa incompleta
+
+const tasks = [
+  { id: 1, title: "Concluir projeto", completed: false, priority: "alta" },
+  { id: 2, title: "Ler emails", completed: true, priority: "média" },
+  { id: 3, title: "Participar da reunião", completed: false, priority: "alta" },
+  { id: 4, title: "Comprar mantimentos", completed: true, priority: "baixa" },
+  { id: 5, title: "Estudar JavaScript", completed: false, priority: "média" },
+];
+
+const primeiraTarefaNaoCompletada = tasks.findIndex(
+  (task) => task.completed === false
+);
+
+console.log(primeiraTarefaNaoCompletada);
